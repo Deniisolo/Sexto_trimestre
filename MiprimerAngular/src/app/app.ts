d304cc0db511ba755f64619f39efce373891e938
Denis Alvarez <127909505+Deniisolo@users.noreply.github.com>
2026-03-21 17:24:14 +0000

    realice intepolation str

--- MiprimerAngular/src/app/app.ts
+++ MiprimerAngular/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('MiprimerAngular');
+  Bienvenido = 'Hola Denis';
+
+  Estudiantes = ['Cuadernos', 'Gorras', 'Gafas']
 }
